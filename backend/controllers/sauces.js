91cd718a1e29bf1ddaa07358efdd9123e926df71
RightSideOfHell <kellylloansi@gmail.com>
2022-12-29 20:23:29 +0100

    ignore/keep

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -76,5 +76,5 @@ exports.getAllSauce = (req, res, next) => {
 };
 
 exports.rateSauce = (req, res) => {
-    
+    console.log(req.body.like)
 }
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -12,6 +12,6 @@ router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-// router.post('/:id/like', auth, sauceCtrl.rateSauce);
+router.post('/:id/like', auth, sauceCtrl.rateSauce);
 
 module.exports = router;
